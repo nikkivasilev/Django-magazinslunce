cd9f07eddbf87451bcb6ccf00331dc59785f86dd
nikkivasilev <n.vasilev105@gmail.com>
2023-01-22 20:15:16 +0200

    minor changes

--- magazinslunce/common/validators.py
+++ magazinslunce/common/validators.py
@@ -1,17 +1,16 @@
-from django.core import exceptions
 from django.core.exceptions import ValidationError
 
 
 def validate_only_letters(value):
     for ch in value:
         if not ch.isalpha():
-            raise exceptions.ValidationError('Only letters are allowed')
+            raise ValidationError('Only letters are allowed')
 
 
 def validate_only_numbers(value):
     for ch in value:
         if not ch.isdigit():
-            raise exceptions.ValidationError('Only numbers are allowed')
+            raise ValidationError('Only numbers are allowed')
 
 
 def validate_file_size(value):
